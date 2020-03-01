78780d0d2cfe87d2318da87e8ea2cc00a1b9eeee
zwonH <a.circle.one@gmail.com>
2020-03-01 21:16:35 +0900

    feat: movie API

--- todoApp/src/screens/MovieScreen.js
+++ todoApp/src/screens/MovieScreen.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 const MovieScreen = () => {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(async () => {
+    const result = await axios.get(
+      'https://yts.mx/api/v2/list_movies.json?limit=50'
+    );
+    setMovieList(result['data']['data']['movies']);
+  }, []);
+
   return (
     <View>
       <Text>MOVIE SCREEN</Text>
+      {movieList.length == 0
+        ? <View>
+            <Text>Loading</Text>
+          </View>
+        : null}
+      {movieList &&
+        movieList.map(movie => {
+          console.log(movie.title);
+          return (
+            <View>
+              <Text>
+                {movie.title}
+              </Text>
+            </View>
+          );
+        })}
     </View>
   );
 };
--- todoApp/src/screens/TodoScreen.js
+++ todoApp/src/screens/TodoScreen.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 
 const TodoScreen = () => {
   return (
-    <View>
-      <Text>TODO SCREEN</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.textInput} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+});
 
 export default TodoScreen;
