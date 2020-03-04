d03d5b99a3ad1296f74dc6ca0e50fb8901b68e4a
zwonH <a.circle.one@gmail.com>
2020-03-04 18:49:22 +0900

    feat: movie images added & MovieScreen style

--- todoApp/src/screens/MovieScreen.js
+++ todoApp/src/screens/MovieScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import axios from 'axios';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 const MovieScreen = () => {
   const [movieList, setMovieList] = useState([]);
@@ -16,25 +17,65 @@ const MovieScreen = () => {
   }, []);
 
   return (
-    <View>
-      <Text>MOVIE SCREEN</Text>
-      {movieList.length == 0
-        ? <View>
-            <Text>Loading</Text>
-          </View>
-        : null}
-      {movieList &&
-        movieList.map((movie, index) => {
-          return (
-            <View key={index}>
-              <Text>
-                {movie.title}
-              </Text>
-            </View>
-          );
-        })}
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.scrollContainer}>
+          {movieList.length == 0
+            ? <View>
+                <Text style={styles.txtLoading}>Loading...</Text>
+              </View>
+            : null}
+          {movieList &&
+            movieList.map((movie, index) => {
+              return (
+                <View key={index} style={styles.card}>
+                  <Image
+                    source={{ uri: movie.medium_cover_image }}
+                    style={styles.image}
+                  />
+                  <Text style={styles.txtMovie}>
+                    {movie.title}
+                  </Text>
+                </View>
+              );
+            })}
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '$mainC',
+    paddingTop: '20rem'
+  },
+  scrollContainer: {
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  txtLoading: {
+    fontFamily: '$jose',
+    color: '$pointC',
+    fontSize: '25rem'
+  },
+  card: {
+    width: '$dW *0.8',
+    alignItems: 'center',
+    marginBottom: '30rem'
+  },
+  image: {
+    width: '$dW *0.3',
+    aspectRatio: 1.5 / 2,
+    resizeMode: 'contain',
+    marginBottom: '10rem'
+  },
+  txtMovie: {
+    fontFamily: '$jose',
+    color: '$darkC',
+    fontSize: '18rem'
+  }
+});
 
 export default MovieScreen;
